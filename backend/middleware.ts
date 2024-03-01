2b4e864150842a79e46222b2f73ac11e9eec665a
kevin gaulin <kevin.gaulin@alfredtechnologies.com>
2024-03-01 09:44:08 -0500

    cors

--- backend/middleware.ts
+++ backend/middleware.ts
@@ -1,40 +1,75 @@
-import { NextRequest, NextResponse } from "next/server";
+// Imports
+// ========================================================
+import { NextResponse, type NextRequest } from "next/server";
 
-const allowedOrigins = ["*"];
+// Config
+// ========================================================
+const ALLOWED_METHODS = "GET, POST, PUT, DELETE, OPTIONS";
+const ALLOWED_ORIGIN = "*";
+const ALLOWED_HEADERS = "Content-Type, Authorization";
+const EXPOSED_HEADERS = "";
+const MAX_AGE = "86400";
+const CREDENTIALS = "true";
 
-const corsOptions = {
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+const corsOptions: {
+  allowedMethods: string[];
+  allowedOrigins: string[];
+  allowedHeaders: string[];
+  exposedHeaders: string[];
+  maxAge?: number;
+  credentials: boolean;
+} = {
+  allowedMethods: (ALLOWED_METHODS ?? "").split(","),
+  allowedOrigins: (ALLOWED_ORIGIN ?? "").split(","),
+  allowedHeaders: (ALLOWED_HEADERS ?? "").split(","),
+  exposedHeaders: (EXPOSED_HEADERS ?? "").split(","),
+  maxAge: (MAX_AGE && parseInt(MAX_AGE)) || undefined, // 60 * 60 * 24 * 30, // 30 days
+  credentials: CREDENTIALS == "true",
 };
-export function middleware(request: NextRequest) {
-  // Check the origin from the request
-  const origin = request.headers.get("origin") ?? "";
 
-  // Handle preflighted requests
-  const isPreflight = request.method === "OPTIONS";
-
-  if (isPreflight) {
-    const preflightHeaders = {
-      "Access-Control-Allow-Origin": origin,
-      ...corsOptions,
-    };
-    return NextResponse.json({}, { headers: preflightHeaders });
-  }
-
-  // Handle simple requests
+// Middleware
+// ========================================================
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  // Response
   const response = NextResponse.next();
 
-  response.headers.set("Access-Control-Allow-Origin", origin);
+  // Allowed origins check
+  const origin = request.headers.get("origin") ?? "";
+  if (
+    corsOptions.allowedOrigins.includes("*") ??
+    corsOptions.allowedOrigins.includes(origin)
+  ) {
+    response.headers.set("Access-Control-Allow-Origin", origin);
+  }
 
-  Object.entries(corsOptions).forEach(([key, value]) => {
-    response.headers.set(key, value);
-  });
+  // Set default CORS headers
+  response.headers.set(
+    "Access-Control-Allow-Credentials",
+    corsOptions.credentials.toString()
+  );
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    corsOptions.allowedMethods.join(",")
+  );
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    corsOptions.allowedHeaders.join(",")
+  );
+  response.headers.set(
+    "Access-Control-Expose-Headers",
+    corsOptions.exposedHeaders.join(",")
+  );
+  response.headers.set(
+    "Access-Control-Max-Age",
+    corsOptions.maxAge?.toString() ?? ""
+  );
 
-  console.log("middleware");
-
+  // Return
   return response;
 }
 
+// See "Matching Paths" below to learn more
 export const config = {
   matcher: "/api/:path*",
 };
